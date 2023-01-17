589a8cfda3365e3127030228ef8dc0a34f6b90df
AbbySantana <96422865+AbbySantana@users.noreply.github.com>
2023-01-17 12:22:34 +0000

    A 5.3 v1

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/index', 'App\Http\Controllers\HomeController@index')->name("home.in
 
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 
-Route::get('/product', 'App\Http\Controllers\ProductController@index')->name("home.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+
