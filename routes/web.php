985db162d8f58f1ad9c43f2eed6cb41329f0fd78
AbbySantana <96422865+AbbySantana@users.noreply.github.com>
2023-01-18 10:41:38 +0000

    A5.3 terminado

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/index', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("products.index");
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("products.show");
 
